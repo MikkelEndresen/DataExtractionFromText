a74d9db15c87f49392e91683630ccbf76b0dcf1d
Mickey <72854733+MikkelEndresen@users.noreply.github.com>
2024-04-26 15:13:07 +1000

    Played with Ollama and Transformers + extract keywords + lines seems to be ok for now

--- src/pipeline/LLM.py
+++ src/pipeline/LLM.py
@@ -4,15 +4,15 @@ from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
 
 def setup_Llama3():
 
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
-    model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 
     tokenizer.save_pretrained('Model/')
     model.save_pretrained('Model/')
 
 def load_model():
 
-    setup_Llama3()
+    #setup_Llama3()
 
     tokenizer = AutoTokenizer.from_pretrained('Model/')
     model = AutoModel.from_pretrained('Model/')
@@ -22,14 +22,50 @@ def load_model():
 
 from transformers import pipeline
 
-if __name__ == "__main__":
-    print("first stage")
-    model, tokenizer = load_model()
+from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+def neo():
 
-    print("second stage")
-    transcriber = pipeline(model=model)
+    model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
+    tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
 
-    print("third stage")
-    print(transcriber("List all numbers between 1 and 10."))
+    prompt = (
+        "In a shocking finding, scientists discovered a herd of unicorns living in a remote, "
+        "previously unexplored valley, in the Andes Mountains. Even more surprising to the "
+        "researchers was the fact that the unicorns spoke perfect English."
+    )
+
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    gen_tokens = model.generate(
+        input_ids,
+        do_sample=True,
+        temperature=0.9,
+        max_length=100,
+    )
+    gen_text = tokenizer.batch_decode(gen_tokens)[0]
+
+from langchain_community.llms import Ollama
+
+def ollama_phi3(query):
+    """
+    Setup: 
+        - Ollama pull phi3
+    """
+
+    llm = Ollama(model='phi3')
+    result = llm.invoke (query)
     
-    print("complete")
+    return result
+
+if __name__ == "__main__":
+    ollama_phi3
+   
+    # model, tokenizer = load_model()
+
+    # print("second stage")
+    # transcriber = pipeline(model=model)
+
+    # print("third stage")
+    # print(transcriber("List all numbers between 1 and 10."))
+    
+    # print("complete")
--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -53,15 +53,56 @@ def create_template():
 
     filepath = "../sample_data/0b8706dc-c9af-4c6b-887d-2f85b5a511e7.txt"
     file = open(filepath, 'r')
-    content = file.read()
-    print(f"Number of characters in document: {len(content)}")
-    print(f"First word just to check {content.split()[0]}")    
+    data = file.read()
+    print(f"Number of characters in document: {len(data)}")
+    print(f"First word just to check {data.split()[0]}")    
 
+    lines = data.split('\n')
+
+    found_method1 = {}
+    found_method2 = []
+    for i, line in enumerate(lines):
+        for word in line.split():
+            if word.lower() in all_keywords:
+                found_method1[word] = line
+                found_method2.append({'word': word, 'line': line})
+
+
+    print(f"Length no overwrite = {len(found_method2)}")
+    print(f"Length overwrite = {len(found_method1.keys())}")
+
+    keys = [key['word'] for key in found_method2]
+    print(keys)
+    print(80*'-')
+    print(found_method1.keys())
+    
+    print(80*'-')
+    # Remove duplicates. All keys == Abrr
+
+    final_found = {}
+
+    for key in found_method1.keys():
+        for x in content:
+            lowercase_synonyms = [item.lower() for item in x['Synonyms']]
+            if key.lower() == x['Abbreviation'].lower() or key.lower() in lowercase_synonyms:
+                final_found[x['Abbreviation']] = found_method1[key]
+                # temp_line = found_method1[key]
+                # del(found_method1[key])
+                # found_method1[x['Abbreviation']] = temp_line
+
+    print(len(final_found.keys()))
+    print(final_found.keys())
+    """
+    Desired format:
+        Only one for each. So bump if already exists...?
+    """
+
+    return final_found
 
     found = []
     for word in content.split():
         word_to_check = word.lower()
-        if word_to_check in all_keywords:
+        if word_to_check in all_keywords: # TODO: This misses two word combinations. e.g: "Troponin I"
             found.append(word.lower())
 
     print(f"Keywords found: {found}")
